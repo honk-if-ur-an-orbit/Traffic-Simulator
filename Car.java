e62741f99b7d891f43a473b10bb0d7a83e3ec717
birb ♪ <birbnaise@gmail.com>
2019-05-01 13:51:30 -0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,9 +1,51 @@
+//car
+import java.util.concurrent.TimeUnit;
 
 public class Car {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	int pos, vel, acc;
+	int trackLength;
+	
+	public Car() {
+		//Starting position
+		pos = 0;
+		//Starting velocity
+		vel = 1;
+		//Starting acceleration
+		acc = 5;
+		
+		trackLength = 40;
 	}
-
+	
+	//drive
+	public void DriveFw() throws InterruptedException {
+		for(int p = pos; p < trackLength; p += 1) {
+			UpdatePos(p);
+			UpdateVel(acc);
+			DrawProgress(vel);
+		}
+	}
+	
+	//update velocity
+	public void UpdateVel(int a) {
+		vel += a;
+	}
+	
+	//update position
+	public void UpdatePos(int p) {
+		pos = p;
+	}
+	
+	//draw car
+	public void DrawProgress(int speed) throws InterruptedException {
+			System.out.print("  o");
+			TimeUnit.MILLISECONDS.sleep(200 / (1 + (speed / 10)));
+	}
+	
+	///////////////////////////////////////////////
+	public static void main(String[] args) throws InterruptedException {
+		Car drive = new Car();
+		drive.DriveFw();
+	}
+	
 }
